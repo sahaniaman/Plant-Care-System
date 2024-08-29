074268d1e8630d5ba52b9665bdc5a68a5adb2828
Aman Sahani <88476684+sahaniaman@users.noreply.github.com>
2024-08-29 20:27:50 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -245,5 +245,6 @@ def predict():
     return jsonify({'error': 'Invalid request'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)   
+    app.run(host='0.0.0.0', port=8080)   
+    app.run(debug=True)   
 
