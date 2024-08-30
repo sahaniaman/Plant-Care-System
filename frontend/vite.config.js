3967d478d56637044fd6facb059c631a6c87671d
Aman Sahani <88476684+sahaniaman@users.noreply.github.com>
2024-08-30 23:05:23 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -12,7 +11,5 @@ export default defineConfig({
         secure: true,
       },
     },
-    port: 8080,  // Change port if needed
-    https: true, // Set to true if using HTTPS for development
   },
 });
